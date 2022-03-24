20da5738120de40ea694dee32bca6da42f73bf7f
Griffin Johnson <gjohnson@supplyframe.com>
2022-03-23 18:55:29 -0700

    update env variable

--- config/db.js
+++ config/db.js
@@ -3,12 +3,10 @@ const CONFIG = config();
 const env = Deno.env.toObject();
 const db_config = {
     host: CONFIG.db_host || env.db_host,
-    username: CONFIG.db_user || env.user,
+    username: CONFIG.db_user || env.db_user,
     password: CONFIG.db_psw || env.db_psw,
     database: CONFIG.db_name || env.db_name,
     port: CONFIG.db_port || env.db_port
 };
-
-console.log(db_config);
 
 export default db_config;
