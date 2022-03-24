25fa6b24b83ed6ee8d7c2a46c8ddc092fef25d55
Griffin Johnson <gjohnson@supplyframe.com>
2022-03-23 18:53:59 -0700

    chagne env getter

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 const CONFIG = config();
+const env = Deno.env.toObject();
 const db_config = {
-    host: CONFIG.db_host || Deno.env.db_host,
-    username: CONFIG.db_user || Deno.env.user,
-    password: CONFIG.db_psw || Deno.env.db_psw,
-    database: CONFIG.db_name || Deno.env.db_name,
-    port: CONFIG.db_port || Deno.env.db_post
+    host: CONFIG.db_host || env.db_host,
+    username: CONFIG.db_user || env.user,
+    password: CONFIG.db_psw || env.db_psw,
+    database: CONFIG.db_name || env.db_name,
+    port: CONFIG.db_port || env.db_port
 };
 
 console.log(db_config);
