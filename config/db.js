bdd140553c1a3eec90f8752dbf24169ef35676df
Griffin Johnson <gjohnson@supplyframe.com>
2022-03-23 18:44:08 -0700

    default to env in config

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 const CONFIG = config();
 const db_config = {
-    host: CONFIG.db_host,
-    username: CONFIG.db_user,
-    password: CONFIG.db_psw,
-    database: CONFIG.db_name,
-    port: CONFIG.db_port
+    host: CONFIG.db_host || Deno.env.db_host,
+    username: CONFIG.db_user || Deno.env.user,
+    password: CONFIG.db_psw || Deno.env.db_psw,
+    database: CONFIG.db_name || Deno.env.db_name,
+    port: CONFIG.db_port || Deno.env.db_post
 };
 
 export default db_config;
