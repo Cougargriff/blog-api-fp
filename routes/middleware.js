893d288c8399619572a36da54cef8488f61b4fdd
Griffin Johnson <cougargriff@gmail.com>
2022-03-28 12:46:52 -0700

    Update middleware.js

--- routes/middleware.js
+++ routes/middleware.js
@@ -1,4 +1,4 @@
-import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+import { abcCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import * as R from "https://deno.land/x/ramda@v0.27.2/mod.ts";
 import logger from '../lib/logger.js';
 
@@ -11,7 +11,7 @@ const logRequest = (next) => (req) => {
 const applyMiddleware = R.invoker(1, 'use');
 const useMiddleware = R.compose(
     applyMiddleware(logRequest),
-    applyMiddleware(oakCors)
+    applyMiddleware(abcCors())
 );
 
 export default useMiddleware;
